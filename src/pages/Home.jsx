a51a4a255245cc4684fa6c7afda52f4ba6b69b67
bypass <zorigreene@gmail.com>
2025-04-27 06:02:48 -0400

    changes to text size on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -72,9 +72,9 @@ return (
       backgroundPosition: "center",
     }}
   >
-    <h1 className="text-5xl font-bold mb-4">Welcome to PQ-ACE</h1>
-    <p className="text-xl mb-4">Where Past Questions Meet Future Success.</p>
-    <p className="text-lg mb-8">
+    <h1 className="text-6xl font-bold mb-4">Welcome to PQ-ACE</h1>
+    <p className="text-2xl mb-4">Where Past Questions Meet Future Success.</p>
+    <p className="text-xl mb-8">
       A space where students can share resources to enhance their learning and prepare for exams, tests, and quizzes.
     </p>
     {!user && (
